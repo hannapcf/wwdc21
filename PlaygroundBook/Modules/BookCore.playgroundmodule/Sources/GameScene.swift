0ff869d368476d191ff5aaaf90f24250d06feb94
hannapcf <hanna.pcf@gmail.com>
2021-04-12 12:30:52 -0300

    modifications

--- PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/GameScene.swift
+++ PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/GameScene.swift
@@ -19,6 +19,7 @@ public class GameScene: SKScene {
     private var stars = [SKSpriteNode]()
     private var lastStarTap: Double?
     private var nextStarMinTime: Double?
+    private var currentStarIndex = 0
     
     // Essa parte do código é executada logo que a cena começa
     public override func didMove(to view: SKView) {
@@ -49,6 +50,7 @@ public class GameScene: SKScene {
         }
         
         view.showsFPS = true
+        createStarPath(starCount: 4)
                 
         
     }
@@ -120,23 +122,44 @@ public class GameScene: SKScene {
     
     func touchDown(atPoint pos : CGPoint) {
         //verificar se a pessoa tocou na primeira estrela
-        
+        if nextStar?.contains(pos) ?? false {
             //se sim:código
             //salva uma variavel o momento de agora para comparar com o toque da proxima estrela
-        
-        emitters = [] //lista vazia
-        //Detectar que a pessoa clicou
-        spawnEmiter(pos: pos)
-        
-        //Criar lista vazia temporária de toques
-        
+            lastStarTap = Date().timeIntervalSince1970 //salvando no ultimo tap o momento de agora
+            emitters = [] //lista vazia
+            spawnEmiter(pos: pos)
+            nextStar?.alpha = 0
+            currentStarIndex += 1
+            nextStar = stars[currentStarIndex]
+            nextStar?.alpha = 1
+            
+        }
+
 
     }
     
     func touchMoved(toPoint pos : CGPoint) {
         //verifica se a pessoa está se aproximando da próxima estrela
+        //fazer depois
             //se sim: colocar emittetr na posição e verfiica se chegou na próxima estrela (tempo maior do que minimo: se menor: desmonta a aurora atual)
-                //se chegou na proxima estrela: habilita proxima estrela/caminho e reseta o tempo
+            if nextStar?.contains(pos) ?? false {
+
+                //salva uma variavel o momento de agora para comparar com o toque da proxima estrela
+                lastStarTap = Date().timeIntervalSince1970 //salvando no ultimo tap o momento de agora
+                spawnEmiter(pos: pos)
+                nextStar?.alpha = 0
+                if currentStarIndex < stars.count-1{
+                    currentStarIndex += 1
+                    nextStar = stars[currentStarIndex]
+                    nextStar?.alpha = 1
+                }
+                else{
+                    //verifica se a pessoa passou em todas as estrelas
+                        //se sim: prepara a remoção das particulas e faz coisas bonitas
+                }
+
+            }
+
             //se nao: verifica se a pessoa esta se afastando da próxima estrela
                 //se sim: desmonta a aurora atual
         
@@ -144,17 +167,25 @@ public class GameScene: SKScene {
         
     }
     
+    fileprivate func destroyAurora() {
+        for emitter in emitters{
+            removeEmitter(emitter)
+        }
+        for star in stars{
+            star.removeFromParent()
+        }
+        emitters = []
+        stars = []
+        nextStar = nil
+        lastStarTap = nil
+        createStarPath(starCount: Int.random(in: 3...5))
+    }
+    
     func touchUp(atPoint pos : CGPoint) {
         //verifica se a pessoa passou em todas as estrelas
-            //se sim: prepara a remoção das particulas e faz coisas bonitas
-            //se nao: desmonta a aurora atual
-        
-//        for emitter in emitters{
-//            let wait = SKAction.wait(forDuration: 6)
-//            emitter.run(wait) {
-//                emitter.removeFromParent()
-//            }
-//        }
+        if currentStarIndex < stars.count{
+            destroyAurora()
+        }
 
     }
     
@@ -180,18 +211,37 @@ public class GameScene: SKScene {
         
         //Criar lista vazia temporária de toques
         emitters.append(emitter) //criando nova lista só com emitter
-        removeEmitter(emitter)
         // Verificar a distancia da proxima estrela
             // Colocar a próxima partícula na cena e na lista
     }
     
     //createStarPath
-    func createStarPath(){
+    func createStarPath(starCount: Int){
+        
+        
         //criar lista de posições aleatórias para ser o caminho de estrelas
+        var randomPositions: [CGPoint] = []
+        for _ in 0..<starCount {
+            let randomPosition = CGPoint(x: CGFloat.random(in: -150...150), y: CGFloat.random(in: -150...150))
+            randomPositions.append(randomPosition)
+        }
+        
         //colocar assets da estrelas com alpha 0
+        for randomPosition in randomPositions{
+            let star = SKSpriteNode(imageNamed: "spark")
+            stars.append(star)
+            star.position = randomPosition
+            star.zPosition = 5
+            star.setScale(0.2)
+            star.alpha = 0
+            addChild(star)
+        }
+        
         //constrói linhas entre as estrelas
         //mostra as duas primeiras estrelas e a linha
-        
+        stars[0].alpha = 1
+        nextStar = stars.first
+        currentStarIndex = 0
     }
     
     
